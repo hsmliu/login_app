df30189bf92923be2624b304f361212f79e43b20
liuwc <lwcjobs@163.com>
2025-01-14 23:29:08 +0800

    新增初级登录和注册页面

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:login_app/custom_button.dart';
 import 'package:login_app/custom_field.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -73,10 +74,119 @@ class LoginScreen extends StatelessWidget {
                   CustomTextField(
                       icon: CupertinoIcons.lock,
                       hint: 'Password',
+                      isPassword: true,
                       gradientColors: [
                         Color(0xFF4A154B),
                         Color(0xFF6B1A6B),
                       ]),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            FadeInUp(
+              duration: Duration(milliseconds: 600),
+              delay: Duration(milliseconds: 400),
+              child: CustomButton(onPressed: () {}, text: 'Log In'),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            FadeIn(
+              duration: Duration(milliseconds: 600),
+              delay: Duration(milliseconds: 600),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account? ",
+                    style: TextStyle(color: Color(0xFF1D1C1D)),
+                  ),
+                  GestureDetector(
+                    onTap: onSingUpTap,
+                    child: Text('Sign Up',
+                      style: TextStyle(
+                          color: Color(0xFF4A154B),
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            FadeInUp(
+              duration: Duration(milliseconds: 600),
+              delay: Duration(milliseconds: 800),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Container(
+                            height: 1,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Colors.transparent,
+                                  Color(0xFFE0E0E0),
+                                ]
+                              ),
+                            ),
+                          ),
+                      ),
+                      Padding(padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Text('Or continue with', style: TextStyle(color: Color(0xFF1D1C1D), fontWeight: FontWeight.w500),),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 1,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: [
+                                  Colors.transparent,
+                                  Color(0xFFE0E0E0),
+                                ]
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElasticIn(
+                        duration: Duration(milliseconds: 800),
+                        delay: Duration(milliseconds: 1000),
+                        child: _buildSocialButton(
+                          icon: Icons.g_mobiledata,
+                          label: 'Google',
+                          gradientColors: [
+                            Color(0xFFDB4437),
+                            Color(0xFFF66D5B),
+                          ]
+                        ),
+                      ),
+                      ElasticIn(
+                        duration: Duration(milliseconds: 800),
+                        delay: Duration(milliseconds: 1200),
+                        child: _buildSocialButton(
+                            icon: Icons.apple,
+                            label: 'Apple',
+                            gradientColors: [
+                              Color(0xFF000000),
+                              Color(0xFF2C2C2C),
+                            ]
+                        ),
+                      )
+                    ],
+                  )
                 ],
               ),
             )
@@ -88,4 +198,35 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
+  Widget _buildSocialButton({
+    required IconData icon,
+    required String label,
+    required List<Color> gradientColors,
+}) {
+    return Container(
+      height: 55,
+      width: 150,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [
+          gradientColors[0].withOpacity(0.1),
+          gradientColors[1].withOpacity(0.1),
+        ],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: gradientColors[0]),
+          SizedBox(width: 8),
+          Text(label, style: TextStyle(
+            color: gradientColors[0],
+            fontWeight: FontWeight.w600
+          ),)
+        ],
+      ),
+    );
+  }
 }
